f7a92f95ed3c386b8f4ffcfa7ca2b0c78082d134
Godwinsham <godwinelijah0102@gmail.com>
2024-07-06 23:53:23 +0530

    intial commit

---
+++ src/main.tsx
@@ -0,0 +1,28 @@
+export { default as Switch } from '../src/components/theme-switch/ThemeSwitch';
+
+export { default as ThemeProvider } from '../src/components/theme-context/ThemeContext';
+
+export { default as Textbox } from '../src/components/textbox/Textbox';
+
+export { default as Datebox } from '../src/components/date/Date';
+
+export { default as Dropdown } from '../src/components/dropdown/Dropdown';
+
+export { default as Radiobutton } from '../src/components/radiobutton/Radiobutton';
+
+export { default as Button } from '../src/components/button/Button';
+
+export { default as Checkbox } from '../src/components/checkbox/Checkbox';
+
+export {default as Icon} from '../src/components/icon/Icon';
+
+export { default as Snackbar } from '../src/components/snackbar/Snackbar';
+export type {
+  ISnackBarOptions,
+  ISnackBar,
+} from '../src/components/snackbar/Snackbar';
+
+export { default as Card } from '../src/components/card/Card';
+
+export { default as Typography } from '../src/components/typography/Typography';
+export { default as Svgheading } from '../src/components/svgheading/Svgheading';
